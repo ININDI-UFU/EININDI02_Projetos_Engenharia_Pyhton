2fea20423cd54cb20494f1ef351ca644b394ec4b
josuemoraisgh <josuemorais@gmail.com>
2025-10-22 08:46:46 -0300

    update file

--- 01_instalLib.bat
+++ 01_instalLib.bat
@@ -2,4 +2,4 @@ CALL pip install -r requirements.txt
 @REM Ou digite
 @REM python -m venv .venv
 @REM . .venv/Scripts/activate
-@REM pip install -r requirements.txt
+@REM pip install -r requirements.txt,
